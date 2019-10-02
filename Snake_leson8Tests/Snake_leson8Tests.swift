33cb3225f8008ea4239dd8a618c3955dd0c2d63b
Lesya <pupochechka@mail.ru>
2019-10-02 17:24:24 +0300

    верно

---
+++ Snake_leson8Tests/Snake_leson8Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Snake_leson8Tests.swift
+//  Snake_leson8Tests
+//
+//  Created by Lesya Os on 29/09/2019.
+//  Copyright © 2019 Lesya Os. All rights reserved.
+//
+
+import XCTest
+@testable import Snake_leson8
+
+class Snake_leson8Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
